4caa8ba017ad4e60ede4c8ccb92c8937f733926b
Nicklas <nnicklas92@gmail.com>
2019-09-26 17:02:34 +0200

    Added Serialization wrapper and switch on deserialized type

---
+++ Dream-Stream/Dream-Stream/Serialization/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using MessagePack;
+
+namespace Producer.Serialization
+{
+    public class Serializer : ISerializer
+    {
+        public byte[] Serialize<T>(T obj)
+        {
+            return LZ4MessagePackSerializer.Serialize(obj);
+        }
+
+        public T Deserialize<T>(byte[] message)
+        {
+            return LZ4MessagePackSerializer.Deserialize<T>(message);
+        }
+    }
+}
